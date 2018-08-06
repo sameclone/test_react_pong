d5b7475ef52301facac45a974dc30251881e6103
Zuyeva Yevgeniya <same_clone@mail.ru>
2018-08-06 20:28:55 +0600

    first commit

---
+++ src/main/java/com/example/monkdemoreact/MonkDemoReactApplication.java
@@ -0,0 +1,12 @@
+package com.example.monkdemoreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkDemoReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkDemoReactApplication.class, args);
+	}
+}
